d73181a679e9115383454eda3dda0949f8001ffa
Ana Martins <ana.af.martins@gmail.com>
2021-08-10 18:01:02 +0200

    iterations 1-7

--- calculator.js
+++ calculator.js
@@ -1,16 +1,35 @@
 function sum(a, b) {
-  return;
+  if (!b) {
+    b = 0;
+  }
+  if (!a) {
+    a = 0;
+  }
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if (b === undefined) {
+    b = 0;
+  }
+  if (a === undefined) {
+    a = 0;
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === undefined) b = 0;
+  if (a === undefined) a = 0;
+   if (b === 0) {
+     throw new Error('Division by 0 is not allowed');
+   }
+  return a / b;
 }
 
 function multiply(a, b) {
+  if (b === undefined) b = 0;
+  if (a === undefined) a = 0;
   return a * b;
 }
 
